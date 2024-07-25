67ca9ee8ad0540ddfbcb3c0819767a15feccbd95
Evgeniy Ivanov <irelion_synergy@mail.ru>
2024-07-25 15:06:14 +0300

    BEM-0011: final template

--- assets/js/swiper.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",
-    spaceBetween: 16,
-    freeMode: false,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
